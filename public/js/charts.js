de53ef3772091fc02441503384f7b55e40e75616
tillber <33751240+tillber@users.noreply.github.com>
2019-04-24 16:26:08 +0200

    made changes to charts.js and installed ibm_db module

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,58 +1,58 @@
 /* Socket setup. */
 var socket = io();
 var chartData = null;
-var myChart = null;
+
+var ctx = document.getElementById('myChart').getContext('2d');
+myChart = new Chart(ctx, {
+	type: 'bar',
+	data: {
+		labels: ['Moisture (%)', 'pH (pH)', 'Temp (°C)'],
+		datasets: [{
+			label: 'Actual',
+			data: [0, 0, 0],
+			backgroundColor: [
+				'rgba(255, 99, 132, 0.8)',
+				'rgba(255, 99, 132, 0.8)',
+				'rgba(255, 99, 132, 0.8)'
+			],
+			borderColor: [
+				'rgba(255, 99, 132, 1)',
+				'rgba(255, 99, 132, 1)',
+				'rgba(255, 99, 132, 1)'
+			],
+			borderWidth: 1
+		},
+		{
+			label: 'Preset',
+			data: [12, 8, 3],
+			backgroundColor: [
+				'rgba(54, 162, 235, 0.8)',
+				'rgba(54, 162, 235, 0.8)',
+				'rgba(54, 162, 235, 0.8)'
+			],
+			borderColor: [
+				'rgba(54, 162, 235, 1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(54, 162, 235, 1)'
+			],
+			borderWidth: 1
+		}
+
+		]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		}
+	}
+});
 
 socket.on('connect', function(){
-	console.log('connected')
-	var ctx = document.getElementById('myChart').getContext('2d');
-	myChart = new Chart(ctx, {
-		type: 'bar',
-		data: {
-			labels: ['Moisture (%)', 'pH (pH)', 'Temp (°C)'],
-			datasets: [{
-				label: 'Actual',
-				data: [0, 0, 0],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.8)',
-					'rgba(255, 99, 132, 0.8)',
-					'rgba(255, 99, 132, 0.8)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(255, 99, 132, 1)',
-					'rgba(255, 99, 132, 1)'
-				],
-				borderWidth: 1
-			},
-			{
-				label: 'Preset',
-				data: [12, 8, 3],
-				backgroundColor: [
-					'rgba(54, 162, 235, 0.8)',
-					'rgba(54, 162, 235, 0.8)',
-					'rgba(54, 162, 235, 0.8)'
-				],
-				borderColor: [
-					'rgba(54, 162, 235, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(54, 162, 235, 1)'
-				],
-				borderWidth: 1
-			}
-
-			]
-		},
-		options: {
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: true
-					}
-				}]
-			}
-		}
-	});
+	console.log('connected');
 });
 socket.on('broadcast', function(freshData){
 	myChart.data.datasets[0].data = freshData;
